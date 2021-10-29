c7cfd50ce64afe029261b62695bdc1dc6d4e5e74
Pablo Mochon <pimubiweb@gmail.com>
2021-10-29 12:00:28 +0200

    task3 ahora es task3-4-5-6-7 completos

--- unit3/wsOPP/task3.js
+++ unit3/wsOPP/task3.js
@@ -1,5 +1,5 @@
 class Lambdasian {
-    constructor(objDatos) { 
+    constructor(objDatos) { //le paso objeto JSON
         this.name = objDatos.name;
         this.age = objDatos.age;
         this.location = objDatos.location;
@@ -10,12 +10,36 @@ class Lambdasian {
     }
 }
 
-datos = {
+datos_alumno = { //objeto JSON que es como los diccionarios en Python ==> clave : valor
     name: "Pablo",
     age: 20,
     location: "IES Ayala"
 }
 
+var miLamb = new Lambdasian(datos_alumno); //creo mi objeto lambdasian
 
-var miLamb = new Lambdasian(datos);
-document.write(miLamb.name + " "+ miLamb.age + " " + miLamb.location);
+document.write("Esta es miLamb: "+ miLamb.name + " "+ miLamb.age + " " + miLamb.location+"\n");
+
+class Instructor extends Lambdasian {
+    constructor(objDados) {
+        super(objDados.name,objDados.age,objDados.location);
+        this.specialty = specialty;
+        this.languaje = favLanguaje;
+        this.phrase = catchPhrase;
+    }
+
+    demo(subject) {
+        return`Today we are learning about ${subject}`
+    }
+
+    grades(student,subject) {
+        return `${student.name} receives a perfect score in ${subject}`
+    }
+}
+
+datos_instructor = {
+    lamb : miLamb,
+    specialty : "redux",
+    favLanguaje : ["JS","Python","Elm"],
+    catchPhrase : "Dont forget the homies"
+}
