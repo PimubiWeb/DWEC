699d593639f88b5afb2b519b1731a9a9e24f029b
Pablo Mochon <pimubiweb@gmail.com>
2021-11-12 12:17:34 +0100

    pagina

--- unit4-Events/WS4-styles/exercise1/estilosconjs.js
+++ unit4-Events/WS4-styles/exercise1/estilosconjs.js
@@ -1,6 +1,16 @@
-function main(){
-    document.getElementsByTagName("body").style.backgroundcolor = "burlywood";
-    document.getElementById("")
-}
+"tema normal"
 
-window.addEventListener('load',main);
+document.getElementById("1").style.backgroundColor = "skyblue";
+document.getElementById("1").style.fontWeight = "bold";
+document.getElementById("1").style.border = "1px solid black";
+
+document.body.style.background = "#FFFFE0";
+document.getElementById("marco").style.backgroundColor = "yellow";
+document.getElementById("marco").style.border = "1px solid black";
+
+newp = document.createElement("p");
+contentp = document.createTextNode("hola q ase");
+newp.appendChild(newContent);
+
+var currentpar = document.getElementById("2");
+document.body.insertBefore(newp, currentpar);
