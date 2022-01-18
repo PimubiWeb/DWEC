0d201f6501be2eab7781a3a2d1cab4b02567770c
Pablo Mochon <pimubiweb@gmail.com>
2022-01-18 10:19:40 +0100

    todolist mas cosas

---
+++ Unit8-Vue/todolist/README.md
@@ -0,0 +1,23 @@
+Proyecto Vue - Recordatorios
+Haciendo uso de Vue como Framework de desarrollo vamos a realizar una aplicación on-line para la gestión de recordatorios (o "todos").
+
+La aplicación debe:
+
+Dar de alta recordatorios, los cuales tienen: descripcion de la tarea, fecha de creacion y prioridad (alta, media, baja). 
+Mostrar ellistado de tareas dadas de alta.
+Las tareas deben almacenarse en local en el navegador, de tal modo que al acceder a la web transcurrido un tiempo, automáticamente aparezcan las tareas que teníamos dadas de alta previamente.
+Debe ser posible borrar una tarea.
+Debe ser posible cambiar el estado de una tarea (completada o activa). Este cambio conlleva un cambio en la interfaz, apareciendo tachada la entrada correspondiente.
+Debe ser posible mostrar solo las completadas, sólo las activas o todos.
+Debe ser posible borrar todas las tareas completadas.
+Debe mostrarse un mensaje indicando el número total de tareas y cuántas se han completado.
+Se valorará una correcta interfaz, basándote en la imagen que tienes a continuación. Debe prestarse atención a aspectos como la usabilidad de la web.
+Debe estar en producción.
+Todos los puntos anteriores son los mínimos para alcanzar un 5 en el proyecto. El resto de puntos hasta el 10 se obitenen según los siguientes puntos:
+
+(0,8 puntos) Al añadir o borrar tareas se realiza una pequeña animación.
+(0,5 puntos) Debe ser posible cambiar la prioridad de una tarea. Para ello cada tarea muestra un pequeño botón por cada prioridad, que al pusar se actualiza.
+(1 punto) Las tareas se muestran siempre ordenadas por prioridad (alta a baja).
+(0,5 puntos) Cada tarea muestra los minutos pasados desde que se dió de alta.
+(1,7 puntos) El proyecto se realiza con la ayuda de VueCLI y usando componentes.
+(0,5 puntos) Debe ser posible filtrar tareas por nombre, de modo que solo aparezcan aquellas que contienen lo escrito en el campo usado para filtrar.
--- Unit8-Vue/todolist/index.js
+++ Unit8-Vue/todolist/index.js
@@ -1,52 +1,64 @@
-const fecha = new Date();
-const TodoList = {
+const app = {
     data() {
         return {
             tarea: "",
+            descripcion: "",
             tarea_array: [],
-            ok: ""
         }
     },
     methods: {
         metertarea() { //METODO PARA AÑADIR UNA NUEVA TAREA A LA LISTA
-            this.tarea_array.push({nombre: this.tarea,fecha: fecha.toDateString(), completado: false});
+            this.tarea_array.push({
+                nombre: this.tarea,
+                descripcion: this.descripcion,
+                fecha: new Date().toDateString(),
+                prioridad: 0, //0 alta, 1 media, 2 baja
+                completado: false});
 
             //subir al localstorage
-            localStorage.setItem('tarea_array', JSON.stringify(this.tarea_array));
-
-            //cuando le de a borrar que se borre tambien del localstorage
-
+            this.subirlocalstorage();
         },
-        cambiarcompletado(index){ //METODO QUE CAMBIA EL ESTADO BOOLEANO
-            this.tarea_array[index].completado ? this.tarea_array[index].completado = false : this.tarea_array[index].completado = true;
+        cambiarestado(index){ //METODO QUE CAMBIA EL ESTADO BOOLEANO
+            //this.tarea_array[index].completado ? this.tarea_array[index].completado = false : this.tarea_array[index].completado = true;
+            this.tarea_array[index].completado = !this.tarea_array[index].completado;
+            this.subirlocalstorage();
         },
         borrartarea(index){ //METODO QUE BORRA UNA TAREA
             this.tarea_array.splice(index, 1);
+            this.subirlocalstorage();
+        },
+        subirlocalstorage(){
+            //subir al localstorage
+            localStorage.setItem('tarea_array', JSON.stringify(this.tarea_array));
         }
 
     },
     computed: { //CAMPOS CALCULADOS
 
-        sincompletar(){ //METODO CALCULADO QUE SACA TODAS TAREAS INCOMPLETAS
+        activas(){ //metodo calculado que saca todas las ta
             let contador = 0
             this.tarea_array.forEach(t => {
                 t.completado ? "" : contador++;
             });
             return contador;
+        },
+        completadas(){
+            this.tarea_array
         }
         
     },
 
-    beforeMount(){ // Called right before the mounting begins: the render function is about to be called for the first time.
+    mounted(){ // Called right before the mounting begins: the render function is about to be called for the first time.
+
+            //bajar local storage
+            if(JSON.parse(localStorage.getItem('tarea_array')))
+                this.tarea_array = JSON.parse(localStorage.getItem('tarea_array'));
         
-        //bajar local storage
-        if(JSON.parse(localStorage.getItem('tarea_array'))){
-            this.tarea_array = JSON.parse(localStorage.getItem('tarea_array'));
-            this.ok = this.tarea_array.length;
-        }
+        
+
     }
 }
 
 window.onload = () => { 
-    Vue.createApp(TodoList).mount("#app")
+    Vue.createApp(app).mount("#app")
 }
