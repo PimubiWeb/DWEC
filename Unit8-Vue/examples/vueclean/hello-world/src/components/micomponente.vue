32d7908f06f6cb15f85a836c13c7ad7243efe87d
Pablo Mochon <pimubiweb@gmail.com>
2022-01-19 13:04:55 +0100

    vue clean import component

---
+++ Unit8-Vue/examples/vueclean/hello-world/src/components/micomponente.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="hello">
+    <p>
+      MI COMPONENTE
+    </p>
+    <p>{{count}}</p>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'micomponente',
+  
+  data() {
+            //datos de mi componente como si fuese el constructor de una clase
+            return{
+                count: 0
+            }
+        },
+        props: ["titulo", "color"],
+        template: `
+        <h1> {{titulo}} - {{color}} </h1>
+        <button @click="count++">
+            you click me {{count}} times.
+        </button>
+        <hr>
+        `
+
+}
+</script>
---
+++ Unit8-Vue/examples/vueclean/hello-world/src/components/nota/index.vue
@@ -0,0 +1,4 @@
+<template src="./nota.html"></template>
+<script src="./nota.js"></script>
+<style src="./nota.scss" scoped lang="scss"></style>
+
