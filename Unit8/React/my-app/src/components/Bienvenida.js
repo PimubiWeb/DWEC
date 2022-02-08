1df102274bd9ca15e3c293305bd5c034048b1a03
Pablo Mochon <pimubiweb@gmail.com>
2022-02-08 17:11:55 +0100

    key index

--- Unit8/React/my-app/src/components/Bienvenida.js
+++ Unit8/React/my-app/src/components/Bienvenida.js
@@ -2,14 +2,31 @@ import React from'react'
 class Bienvenida extends React.Component {
     mensaje = 'tukuntakan'
 
-    constructor(props){
-        super(props)
-        // this.atributos = props;
-    }
+    nombres = [{nombre: 'jose', edad: 13},
+                {nombre: 'mikel', edad:22}];
+    
+
+    // constructor(props){
+    //     super(props)
+    //     // this.atributos = props;
+    //     //para que no se pierda el this
+    // }
 
     render(){
-            return <h1>Bienvenida comp con mensaje={this.mensaje} <br></br> 
-            titulo: {this.props.titu}</h1>
+        const listItems = this.nombres.map((o,index)=> //para cada elemento del array necesita tener una key unica como puede ser el index
+        <li key={index}>{o.nombre} que tiene {o.edad}</li>
+        )
+
+
+        return <div>Bienvenida comp con mensaje={this.mensaje} con titulo: {this.props.titu}
+        
+        
+
+        <ol>
+            {listItems}
+        </ol>
+        
+       </div>
     }
 }
 
